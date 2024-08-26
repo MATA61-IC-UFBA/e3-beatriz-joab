c9a4ea71ab2307eb908f1fbf96ca201e9684a7b2
Joab Guimarães <38448708+joabpgb@users.noreply.github.com>
2024-08-26 13:40:39 -0300

    Update token.h

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,13 +1,18 @@
 /* token.h */
 
 typedef enum {
-        ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
-        CHAR,
-	STR,
-	ERROR
-} token_t; 
-
+    ID = 256, 
+    NUM,
+    IF,
+    ELSE,
+    WHILE,
+    FOR,
+    RETURN,
+    INT,
+    FLOAT,
+    LEQ,     // <=
+    GEQ,     // >=
+    EQ,      // ==
+    NEQ,     // !=
+    ERROR    // Token de erro para símbolos não reconhecidos
+} token_t;
