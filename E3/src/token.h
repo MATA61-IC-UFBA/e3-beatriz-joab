fa5e0907d45571a4e8e240cc69b6edcc08dfbd0b
Beatriz Morais <beatriz.morais@reidopitaco.com>
2024-08-30 23:48:11 -0300

    feat: ajustes pequenos

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,18 +1,20 @@
 /* token.h */
 
 typedef enum {
-    ID = 256, 
-    NUM,
-    IF,
-    ELSE,
-    WHILE,
-    FOR,
-    RETURN,
-    INT,
-    FLOAT,
-    LEQ,     // <=
-    GEQ,     // >=
-    EQ,      // ==
-    NEQ,     // !=
-    ERROR    // Token de erro para símbolos não reconhecidos
+    ID = 256,          
+    NUM,        
+    IF,               
+    ELSE,             
+    WHILE,          
+    FOR,              
+    RETURN,           
+    INT,            
+    FLOAT,            
+    LEQ,             
+    GEQ,         
+    NEQ,             
+    EQ,                
+    CHAR,       
+    STR,               
+    ERROR              
 } token_t;
